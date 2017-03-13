e2c06728dc6074f70e15bd53938ac0e89a8f55f0
Konovod <konovod@gmail.com>
2017-03-13 19:33:00 -0100

    spec for raising on bad keys

--- spec/monocypher_spec.cr
+++ spec/monocypher_spec.cr
@@ -58,6 +58,11 @@ describe Crypto do
 
     shared1.should eq shared2
     shared1.should_not eq shared3
+
+    public_bad = Crypto::PublicKey.new("0000000000000000000000000000000000000000000000000000000000000000")
+    expect_raises(Exception) do
+      Crypto::SymmetricKey.new(secret: secret1, public: public_bad)
+    end
   end
 
   it "sign messages" do

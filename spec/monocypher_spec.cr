422fb8daadecf5c1ffb3396a0d04ab7893fc4a17
Konovod <konovod@gmail.com>
2018-01-31 22:31:10 -0100

    looks like argon works too

--- spec/monocypher_spec.cr
+++ spec/monocypher_spec.cr
@@ -11,7 +11,7 @@ describe Crypto do
     salt1.compare(nonce).should be_false
   end
 
-  pending "hashes password" do
+  it "hashes password" do
     pass = "12345678"
     salt = Crypto::Salt.new
     key1 = Crypto::SecretKey.new(password: pass, salt: salt)
